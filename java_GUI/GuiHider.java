c663b7f16803b4ca9e23a4a96a5a8bbd3d97052b
Gempio <mmusialek1993@gmail.com>
2016-04-05 00:49:21 +0100

    Started implementing treasure hunt

--- java_GUI/GuiHider.java
+++ java_GUI/GuiHider.java
@@ -3,26 +3,19 @@ import java.util.*;
 public class GuiHider {
 
 	static HashMap<Integer,String[]> treasures;
-
+	
 	public static void main(String[] args) {
 
 		treasures = new HashMap<Integer,String[]>();
-		String[] temp = {"Red","Ball"};
-		treasures.put(0, temp);
-		temp = {"Blue","Hat"};
-		treasures.put(1, temp);
-		temp = {"Red", "Square"};
-		treasures.put(2, temp);
-		temp = {"Green", "Hat"};
-		treasures.put(3, temp);
-		temp = {"Yellow","Bottle"};
-		treasures.put(4, temp);
-		temp = {"Orange", "Bottle"};
-		treasures.put(5, temp);
-		temp = {"Pink", "Hat"};
-		treasures.put(6, temp);
-		temp = {"Violet", "Square"};
-		treasures.put(7, temp);
+		
+		treasures.put(0, setTreasure("Red","Ball"));
+		treasures.put(1, setTreasure("Blue","Hat"));
+		treasures.put(2, setTreasure("Red", "Square"));
+		treasures.put(3, setTreasure("Green", "Hat"));
+		treasures.put(4, setTreasure("Yellow","Bottle"));
+		treasures.put(5, setTreasure("Orange", "Bottle"));
+		treasures.put(6, setTreasure("Pink", "Hat"));
+		treasures.put(7, setTreasure("Violet", "Square"));
 
 
 		Connector r = new Connector("127.0.1.1", 6009);
@@ -52,6 +45,9 @@ public class GuiHider {
 	public static void sendRobot(Connector r, int robot, int room) {
 		r.sendMessage("%%goto TabUI SimR " + robot + " " + room + " " + room + " 45");
 	}
-
-
+	//A method that returns an array of two property treasure.
+	public static String[] setTreasure(String colour, String shape) {
+		String[] temp = {colour,shape};
+		return temp;
+	}
 }
