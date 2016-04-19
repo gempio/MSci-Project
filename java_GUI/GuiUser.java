29e5c48c62234b72bda18ee54eda9adfb3cf6a6f
gempio <mmusialek1993@gmail.com>
2016-04-19 23:56:21 +0100

    Pulling the dialogue out of UI and backing up the report

--- java_GUI/GuiUser.java
+++ java_GUI/GuiUser.java
@@ -317,53 +317,7 @@ public class GuiUser implements Listener{
 
 		public void createDialogs(String attributes) {
 			//Function for later.
-			if(attributes.contains("Nothing")){}
-			String[] temp = attributes.split("\\(");
-			attributes = temp[1] + " " + temp[2];
-			String[] optionsForTreasure= {"Take Picture", "Grab Treasure", "Continue"};
-			final JOptionPane optionPane = new JOptionPane(
-                                    attributes,
-                                    JOptionPane.QUESTION_MESSAGE,
-                                    JOptionPane.YES_NO_CANCEL_OPTION);
-
-			optionPane.setOptions(optionsForTreasure);
-            final JDialog dialog = new JDialog(this,
-                                         "Identified a shape",
-                                         true);
-            dialog.setContentPane(optionPane);
-            dialog.setDefaultCloseOperation(
-                JDialog.DO_NOTHING_ON_CLOSE);
-            dialog.addWindowListener(new WindowAdapter() {
-                public void windowClosing(WindowEvent we) {
-                    System.out.println("Thwarted user attempt to close window.");
-                }
-            });
-            optionPane.addPropertyChangeListener(
-                new PropertyChangeListener() {
-                    public void propertyChange(PropertyChangeEvent e) {
-                        String prop = e.getPropertyName();
-
-                        if (dialog.isVisible()
-                         && (e.getSource() == optionPane)
-                         && (JOptionPane.VALUE_PROPERTY.equals(prop))) {
-                            dialog.setVisible(false);
-                        }
-                    }
-                });
-            dialog.pack();
-            dialog.setLocationRelativeTo(this);
-            dialog.setVisible(true);
-
-            String value = (String) optionPane.getValue();
-            if (value.equals("Take Picture")) {
-                takePicture(attributes);
-            } else if (value.equals("Grab Treasure")) {
-                System.out.println("Try using the window decorations "
-                         + "to close the non-auto-closing dialog. "
-                         + "You can't!");
-            } else {
-                
-            }
+			new GuiDialog();
 		}
 
 		public void takePicture(String attributes) {
